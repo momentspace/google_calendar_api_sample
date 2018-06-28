58f8b4a3a40aaa4fa7b57353d7bcf21fc02b7d80
momentspace <momentspace@gmail.com>
2018-06-28 17:27:09 +0900

    feat. add sample calendar and sample event

--- quickstart.rb
+++ quickstart.rb
@@ -2,12 +2,13 @@ require 'google/apis/calendar_v3'
 require 'googleauth'
 require 'googleauth/stores/file_token_store'
 require 'fileutils'
+require 'date'
 
 OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
 APPLICATION_NAME = 'Google Calendar API Ruby Quickstart'.freeze
 CLIENT_SECRETS_PATH = 'secrets/client_secrets.json'.freeze
 CREDENTIALS_PATH = 'token.yaml'.freeze
-SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
+SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR
 
 ##
 # Ensure valid credentials, either by restoring from the saved credentials
@@ -51,3 +52,53 @@ response.items.each do |event|
   start = event.start.date || event.start.date_time
   puts "- #{event.summary} (#{start})"
 end
+
+TIME_ZONE = 'Japan'
+
+# 専用カレンダーを探す
+calendar_id = 'private'
+if File.exists?('calendar.id')
+  revo_id = File.read('calendar.id')
+  begin
+    # event = Google::Apis::CalendarV3::Event.new(event_data)
+    service.get_calendar(revo_id)
+    calendar_id = revo_id
+  rescue => e
+    # calendar not found
+    puts 'calendar was deleted.'
+    exit
+  end
+
+else
+  calendar = Google::Apis::CalendarV3::Calendar.new(
+    summary: 'リネレボ用',
+    time_zone: TIME_ZONE
+  )
+  result = service.insert_calendar(calendar)
+  File.write('calendar.id', result.id)
+  calendar_id = result.id
+end
+
+# 登録するイベント内容
+start_time = DateTime.now
+end_time = DateTime.now + 1
+event_data = {
+  summary: 'サマリ',
+  description: 'description',
+  location: 'location',
+  start: {
+    date_time: start_time.iso8601,
+    time_zone: TIME_ZONE
+  },
+  end: {
+    date_time: end_time.iso8601,
+    time_zone: TIME_ZONE
+  }
+}
+
+
+# 登録
+event = Google::Apis::CalendarV3::Event.new(event_data)
+response = service.insert_event(calendar_id, event)
+p response
+
